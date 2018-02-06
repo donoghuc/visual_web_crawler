95385e48da981a1f7a5154a0044ba29e8fe65908
David Hartman <dhartman1.dh@gmail.com>
2018-02-05 20:48:43 -0500

    add form to /search to show previous searches if there are any

--- crawler_app/crawler_app/controllers/search_controller.py
+++ crawler_app/crawler_app/controllers/search_controller.py
@@ -7,4 +7,5 @@ class SearchController(BaseController):
 
     @pyramid_handlers.action(renderer='templates/search/index.pt')
     def index(self):
-        return {'value': 'SEARCH'}
+        return {'previous_searches': [{'id': '12345', 'url': 'www.google.com'},
+                                      {'id': '12346', 'url': 'www.yahoo.com'}]}
