4ed1fbe17155e3904e10fce2749ecc25a770bf4a
ngntrn <trannga@oregonstate.edu>
2018-02-23 11:24:31 -0600

    comments

--- Crawler/Spider.py
+++ Crawler/Spider.py
@@ -7,29 +7,35 @@ import sys
 MAX_URLS = 30
         
 class Spider:
-    def __init__(self, seed_url, search_type, limit, keyword=None):
+    def __init__(self, seed_url, search_type, depth_limit, keyword=None):
         Page.__init__(self, seed_url)
         self.seed_url = seed_url
         self.search_type = search_type # 'BFS' or 'DFS'
         self.keyword = keyword
-        self.count = 0
-        self.limit = int(limit) # number of pages to crawl (number of nodes)
+        self.count = 0 # track number of links crawled
+        self.depth_limit = int(depth_limit) # max depth to crawl
         self.depth = 0 # current node depth
         self.to_visit = Page.to_visit # list of urls to crawl
         self.visited_set = Page.visited_set   # set of urls already crawled
-        self.graph = Graph()
+        self.graph = Graph() # page nodes graph
         self.id = 0
-        self.stop_crawl = bool()
+        self.stop_crawl = bool() # track if stop keyword is found
         root_node = Page_Node(seed_url, None, 0, None, 0, False)
         self.start(self.search_type, root_node)
     
     #  start the crawling
     def start(self, search_type, node):
+        
+        # initialize data structures
         self.to_visit.clear()
         self.visited_set.clear()
         
+        # keyword not found
         self.stop_crawl == False
+        
+        # add root node to list to crawl
         self.to_visit.append(node)
+        
         if self.search_type == 'BFS':
             self.crawler('BFS', node)
         else:
@@ -39,6 +45,7 @@ class Spider:
     def crawler(self, search_type, node):
         while len(self.to_visit) > 0 and not self.stop_crawl and self.count < MAX_URLS:
             print('crawling')
+            # check if link has already been crawled
             crawled = node.url in self.visited_set
             if not crawled:
                 self.graph.add_node(node, self.id)
@@ -55,17 +62,25 @@ class Spider:
                         self.graph.add_edge(source_node.id, node.id)
                             # set node's parent node
                         node.parent_node = source_node.id
+            
+                # create new Page object
                 pg = Page(node.url)
-                if node.node_depth < self.limit:
+                
+                # if node limit has not been reached
+                if node.node_depth < self.depth_limit:
                     links = pg.get_links(node.url)
                     links = validate_url(links, self.count, MAX_URLS)
                     # remove any duplicate links present
                     links = remove_duplicates(links)
                     self.crawl_links(node, links)
+        
+        # check if stop keyword is found
             if self.keyword and pg.find_keyword(self.keyword):
                 node.found = True
                 self.stop_crawl = True
+                self.end_crawl()
             
+            # get next node to crawl
             if self.count < MAX_URLS:
                 node = self.get_next()
         
@@ -112,6 +127,8 @@ class Spider:
 
     def end_crawl(self):
         self.print_data()
+        
+        # exit if keyword is found
         if self.stop_crawl:
             exit(0)
 
