16be3664c5cba4aefafd989decbd563afec8f3b0
donoghuc <cas.donoghue@gmail.com>
2018-02-23 16:51:39 -0800

    resolve dependencies introduced from crawler and data storage

--- crawler_app/setup.py
+++ crawler_app/setup.py
@@ -9,12 +9,15 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
 requires = [
+    'lxml',
+    'pandas',
     'passlib',
     'plaster_pastedeploy',
     'pyramid',
     'pyramid_chameleon',
     'pyramid_handlers',
     'pyramid_debugtoolbar',
+    'requests',
     'waitress',
     'sqlalchemy'
 ]
