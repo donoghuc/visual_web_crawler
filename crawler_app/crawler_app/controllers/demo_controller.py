e9cf858073dd50c37ee3d113a85c060105c956b4
donoghuc <cas.donoghue@gmail.com>
2018-03-05 20:20:29 -0800

    oops forgot to track new files

---
+++ crawler_app/crawler_app/controllers/demo_controller.py
@@ -0,0 +1,79 @@
+import pyramid_handlers
+from crawler_app.controllers.base_controller import BaseController
+# from crawler_app.infrastructure.supressor import suppress
+prefix = "http://localhost:6543"
+
+class DemoController(BaseController):
+
+    @pyramid_handlers.action(renderer='templates/demo/index.pt')
+    def index(self):
+
+        return {"value": "demo"}
+
+
+    @pyramid_handlers.action(renderer='templates/demo/node.pt',
+                             name="node0")
+    def node_0(self):
+        return {'value': 'cyan', 'links': [prefix + '/demo/node0/node1',
+                       prefix + '/demo/node0/node2',prefix + '/demo/node0/node3']}
+
+
+    @pyramid_handlers.action(renderer='templates/demo/node.pt',
+                             name="node1")
+    def node_1(self):
+        return {'value': 'blue', 'links': []}
+
+
+    @pyramid_handlers.action(renderer='templates/demo/node.pt',
+                             name="node2")
+    def node_2(self):
+        return {'value': 'red', 'links': [prefix + '/demo/node0/node2/node4',
+                                          prefix + '/demo/node0/node2/node5']}
+
+
+    @pyramid_handlers.action(renderer='templates/demo/node.pt',
+                             name="node3")
+    def node_3(self):
+        return {'value': 'yellow', 'links': [prefix + '/demo/node0/node3/node6']}
+
+
+    @pyramid_handlers.action(renderer='templates/demo/node.pt',
+                             name="node4")
+    def node_4(self):
+        return {'value': 'green', 'links': []}
+
+    @pyramid_handlers.action(renderer='templates/demo/node.pt',
+                             name="node5")
+    def node_5(self):
+        return {'value': 'purple', 'links': []}
+
+
+    @pyramid_handlers.action(renderer='templates/demo/node.pt',
+                             name="node6")
+    def node_6(self):
+        return {'value': 'grey', 'links': [prefix + '/demo/node0/node3/node6/node7',
+            prefix + '/demo/node0/node3/node6/node8',prefix + '/demo/node0/node3/node6/node9']}
+
+
+    @pyramid_handlers.action(renderer='templates/demo/node.pt',
+                             name="node7")
+    def node_7(self):
+        return {'value': 'pink', 'links': []}
+
+
+    @pyramid_handlers.action(renderer='templates/demo/node.pt',
+                             name="node8")
+    def node_8(self):
+        return {'value': 'black', 'links': [prefix + '/demo/node0/node3/node6/node8/node10']}
+
+
+    @pyramid_handlers.action(renderer='templates/demo/node.pt',
+                             name="node9")
+    def node_9(self):
+        return {'value': 'white', 'links': []}
+
+
+    @pyramid_handlers.action(renderer='templates/demo/node.pt',
+                             name="node10")
+    def node_10(self):
+        return {'value': 'brown', 'links': []}
