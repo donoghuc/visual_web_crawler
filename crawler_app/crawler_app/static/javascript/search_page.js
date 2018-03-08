3c5a127787d47380f75b1458afb18f9490aeb747
David Hartman <dhartman1.dh@gmail.com>
2018-03-08 11:42:00 -0500

    set bfs depth to 1-3 and dfs depth to 1-20

--- crawler_app/crawler_app/static/javascript/search_page.js
+++ crawler_app/crawler_app/static/javascript/search_page.js
@@ -1,4 +1,8 @@
 
+const BFS_LIMIT = 3;
+const DFS_LIMIT = 20;
+const DEPTH_STR = "Depth Level (From 1 to {num})";
+
 $(".btn-submit").click(function(event) {
   if (this.form.checkValidity()) {
     $("input, select, .btn").css("pointer-events", "none");
@@ -6,3 +10,13 @@ $(".btn-submit").click(function(event) {
   }
   return true;
 });
+
+$("#bfs-btn").click(function(event) {
+  $("#depth-input").prop("placeholder", DEPTH_STR.replace("{num}", BFS_LIMIT))
+      .prop("max", BFS_LIMIT);
+})
+
+$("#dfs-btn").click(function(event) {
+  $("#depth-input").prop("placeholder", DEPTH_STR.replace("{num}", DFS_LIMIT))
+      .prop("max", DFS_LIMIT);
+})
